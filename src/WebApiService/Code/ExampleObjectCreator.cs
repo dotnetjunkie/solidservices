898feff6eea4821993fe7c7216625f571343e382
steven.van.deursen <steven.van.deursen@topicus.nl>
2015-11-19 17:05:56 +0100

    Simplified ExampleObjectCreator (again).

--- src/WebApiService/Code/ExampleObjectCreator.cs
+++ src/WebApiService/Code/ExampleObjectCreator.cs
@@ -1,26 +1,19 @@
 ﻿using System;
-using System.Linq.Expressions;
-using System.Reflection;
 using Ploeh.AutoFixture;
+using Ploeh.AutoFixture.Kernel;
 
 namespace WebApiService.Code
 {
     public static class ExampleObjectCreator
     {
-        private static readonly MethodInfo CreateMethod =
-            GetMethod(() => SpecimenFactory.Create<int>(new Fixture()))
-            .GetGenericMethodDefinition();
-
         public static object Create(Type type)
         {
             var fixture = new Fixture();
+
             int index = 1;
             fixture.Register(() => "sample text " + index++);
 
-            return CreateMethod.MakeGenericMethod(type).Invoke(null, new object[] { fixture });
+            return new SpecimenContext(fixture).Resolve(type);
         }
-
-        private static MethodInfo GetMethod(Expression<Func<int>> methodCall) => 
-            ((MethodCallExpression)methodCall.Body).Method;
     }
 }
