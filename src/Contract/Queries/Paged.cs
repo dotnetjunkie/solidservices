a40788c5fca5d69639777fea7436242f15c6dc94
dotnetjunkie <github@cuttingedge.it>
2016-04-25 22:32:12 +0200

    Type safety added to DataContract

--- src/Contract/Queries/Paged.cs
+++ src/Contract/Queries/Paged.cs
@@ -4,7 +4,7 @@
 
     // Applying the DataContract attribute to generic types prevents WCF from postfixing the closed-generic 
     // type name with a seemingly random hexadecimal code.
-    [DataContract(Name = "PagedOf{0}")]
+    [DataContract(Name = nameof(Paged<T>) + "Of{0}")]
     public class Paged<T>
     {
         [DataMember] public PageInfo Paging { get; set; }
