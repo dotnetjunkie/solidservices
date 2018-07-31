7ac452f9e2c8553f8ba4b03ecf69b1fba0a24d80
dotnetjunkie <steven@cuttingedge.it>
2018-07-31 22:18:29 +0200

    CORS example added.

--- src/WebApiService/App_Start/WebApiConfig.cs
+++ src/WebApiService/App_Start/WebApiConfig.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using System.Net.Http;
     using System.Web.Http;
+    using System.Web.Http.Cors;
     using System.Web.Http.Description;
     using BusinessLayer;
     using Code;
@@ -14,6 +15,11 @@
     {
         public static void Register(HttpConfiguration config, Container container)
         {
+            // Setting the same-origin policy to 'unrestricted'. Remove or change this line if you want to 
+            // restrict web pages from making AJAX requests to other domains. For more information, see:
+            // https://docs.microsoft.com/en-us/aspnet/web-api/overview/security/enabling-cross-origin-requests-in-web-api#scope-rules-for-enablecors
+            config.EnableCors(new EnableCorsAttribute(origins: "*", headers: "*", methods: "*"));
+
             UseCamelCaseJsonSerialization(config);
 
 #if DEBUG
