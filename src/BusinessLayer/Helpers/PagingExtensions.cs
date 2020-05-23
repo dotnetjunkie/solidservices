d5de75616a83219461b31cc6474bdff8e0d321dc
dotnetjunkie <steven@cuttingedge.it>
2020-05-23 13:09:17 +0200

    Added the possibility to request an unpaged result from a query using the PageInfo DTO.

--- src/BusinessLayer/Helpers/PagingExtensions.cs
+++ src/BusinessLayer/Helpers/PagingExtensions.cs
@@ -6,26 +6,36 @@
 
     public static class PagingExtensions
     {
+        /// <summary>Apply paging in memory, using LINQ to Objects.</summary>
+        /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="paging">The optional paging object. When null, the default paging values will be used.</param>
+        /// <returns>A paged result.</returns>
         public static Paged<T> Page<T>(this IEnumerable<T> collection, PageInfo paging)
         {
             paging = paging ?? new PageInfo();
 
-            return new Paged<T>
-            {
-                Items = collection.Skip(paging.PageIndex * paging.PageSize).Take(paging.PageSize).ToArray(),
-                Paging = paging,
-            };
+            IEnumerable<T> items = paging.IsSinglePage()
+                ? collection
+                : collection.Skip(paging.PageIndex * paging.PageSize).Take(paging.PageSize);
+
+            return new Paged<T> { Items = items.ToArray(), Paging = paging };
         }
 
+        /// <summary>Apply paging using an efficient database query.</summary>
+        /// <typeparam name="T">The type of objects to enumerate.</typeparam>
+        /// <param name="collection">The collection</param>
+        /// <param name="paging">The optional paging object. When null, the default paging values will be used.</param>
+        /// <returns>A paged result.</returns>
         public static Paged<T> Page<T>(this IQueryable<T> collection, PageInfo paging)
         {
             paging = paging ?? new PageInfo();
 
-            return new Paged<T>
-            {
-                Items = collection.Skip(paging.PageIndex * paging.PageSize).Take(paging.PageSize).ToArray(),
-                Paging = paging,
-            };
+            IQueryable<T> items = paging.IsSinglePage()
+                ? collection
+                : collection.Skip(paging.PageIndex * paging.PageSize).Take(paging.PageSize);
+
+            return new Paged<T> { Items = items.ToArray(), Paging = paging };
         }
     }
 }
--- src/Contract/Queries/PageInfo.cs
+++ src/Contract/Queries/PageInfo.cs
@@ -3,10 +3,16 @@
     /// <summary>Object containing information about paging.</summary>
     public class PageInfo
     {
+        /// <summary>Returns a PageInfo that represents the request for a single page.</summary>
+        public static PageInfo SinglePage() => new PageInfo { PageIndex = 0, PageSize = -1 };
+
         /// <summary>The 0-based page index.</summary>
         public int PageIndex { get; set; }
 
         /// <summary>The number of items in a page.</summary>
         public int PageSize { get; set; } = 20;
+
+        /// <summary>Gets the value indicating whether the page info represents the request for a single page.</summary>
+        public bool IsSinglePage() => this.PageIndex == 0 && this.PageSize == -1;
     }
 }
